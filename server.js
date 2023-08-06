4144ae7f95170985be9d0c02b40e21b59de2fdb6
mohithworks <mohithworks2002@gmail.com>
2023-08-06 19:38:40 +0530

    Message

--- server.js
+++ server.js
@@ -10,16 +10,21 @@ app.use(express.json());
 const baseRouter = express.Router();
 
 baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+    return res.send('Hello World!');
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post('/add', (request, res) => {
+    const first = parseInt(request.body.first);
+    const second = parseInt(request.body.second);
+    const sum = first + second;
+    res.json({ "result:-": sum });
 });
 
-
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post('/subtract', (request, res) => {
+    const first = parseInt(request.body.first);
+    const second = parseInt(request.body.second);
+    const diff = first - second;
+    res.json({ "result ": diff });
 });
 
 app.use(baseUrl, baseRouter);
